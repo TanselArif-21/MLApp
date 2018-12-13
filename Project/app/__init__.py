e46a7e8a09528d5892425d4d5fb4bd00f8af26a8
TanselArif-21 <tanselarif@live.co.uk>
2018-12-13 22:51:19 +0000

    Adding the functionality for the user to receive a plot of the data as well

--- Project/app/__init__.py
+++ Project/app/__init__.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, redirect, url_for, flash, jsonify, render_template
-from functions import return_something,return_something2
+from flask import Flask, request, redirect, url_for, flash, jsonify, render_template, send_from_directory
+from functions import return_something, return_something2, get_prediction_lr
 import flask_excel as excel
+import pandas as pd
+import os
+import string
+import random
 
 # This line sets the app directory as the working directory
 app = Flask(__name__)
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # The home route
 @app.route('/', methods=['GET'])
@@ -25,17 +30,44 @@ def dosomething():
 def dosomething2():
     # If the request is a post request
     if request.method == 'POST':
-        return jsonify({"result": request.get_array(field_name='file')})
+        # This line creates a dataframe from a list of lists
+        df = pd.DataFrame(request.get_array(field_name='file'))
+
+        # The first row is the list of column names so set the column names to the first row
+        df.columns = df.iloc[0, :]
+
+        # Now remove the first row
+        df = df[1:]
+
+        # Print the dataframe to the console
+        print(df)
+
+        # Print the column names to the console
+        print(df.columns)
+
+        # These are the values given for the fields
+        input_list = list(request.form.values())
+
+        filename = ''
+        for i in range(4):
+            filename = filename + random.choice(string.ascii_letters)
+
+        # These are the names of the fields, i.e. LotFrontage, LotArea etc...
+        input_names = list(request.form)
+
+        result = get_prediction_lr(df, input_list, filename)
+
+        # This code section creates a dictionary where each key is a row in the dataframe
+        res = dict()
+        for i in range(len(df)):
+            res[i] = list(df.iloc[i, :])
+
+        # Return the result
+        #return 'The predicted Sale Price of this house is: ' + str(round(result, 2)) + '. \n <button><a href="/plot/' + filename + '">See Sales Price Distribution</a></button><p></p>'
+        return 'The predicted Sale Price of this house is: ' + str(round(result, 2)) + '. <p></p> <img src="static/' + filename + '.png"><p></p>'
     else:
-        # Show them a form
-        return '''
-        <!doctype html>
-        <title>Upload an excel file</title>
-        <h1>Excel file upload (csv)</h1>
-        <form action="" method=post enctype=multipart/form-data><p>
-        <input type=file name=file><input type=submit value=Upload>
-        </form>
-        '''
+        # Show the form page
+        return render_template('dosomethingform.html')
 
 
 # A route to the test page that simply returns hello
--- Project/app/functions.py
+++ Project/app/functions.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def return_something():
     return 200
@@ -5,3 +10,44 @@ def return_something():
 
 def return_something2(s):
     return s
+
+
+def plot_df(cols, filename):
+    x = np.random.randn(100)
+    ax = sns.distplot(list(cols))
+    plt.savefig('static/' + filename)
+    return
+
+
+def get_prediction_lr(df, ls, filename):
+    '''
+    Description: This function accepts a dataframe of house prices and a user provided list to predict the sales price for
+    df: a data frame
+    ls: a list containing the information for a house to predict for
+    '''
+
+    # Save a plot image
+    plot_df(df['SalePrice'], filename)
+
+    # This is a linear regression object
+    linreg = LinearRegression()
+
+    # This is our predictors dataframe
+    x = df.drop('SalePrice', axis=1)
+
+    # We fit the model using the predictors
+    linreg.fit(x, df['SalePrice'])
+
+    # This is our observation we would like to predict. Initially we use the means of all observations
+    # and we overwrite the fields which are provided by the user. i.e. the default of a field
+    # is the mean for that field
+    pred_x = x.mean().values.reshape(1, -1)
+
+    # Loop through the user provided list of fields and update the list for our observation
+    # whenever a field is supplied
+    for i in range(len(ls)):
+        if ls[i] != '':
+            pred_x[0][i] = float(ls[i])
+
+    # Return the predicted Sale Price
+    return list(linreg.predict(pred_x))[0]
